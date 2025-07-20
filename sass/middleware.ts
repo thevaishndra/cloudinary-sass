74f68dc994fe1039279866c2f81eea0b962cd756
thevaishndra <chandrav306m@gmail.com>
2025-07-20 13:03:32 +0530

    clerk configuration

---
+++ sass/middleware.ts
@@ -0,0 +1,45 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signup",
+    "/",
+    "/home"
+])
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export default clerkMiddleware((auth, req) => {
+    const {userId} = auth();
+    const currentUrl = new URL(req.url)
+    const isAccessingDashboard = currentUrl.pathname === "/home";
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+    if(userId && isPublicRoute(req) && !isAccessingDashboard){
+        return NextResponse.redirect(new URL("/home", req.url))
+    }
+
+    //not logged in
+    if(!userId){
+        //if user is not logged in and trying to access a protectedd route
+        if(!isPublicRoute(req) && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/signin", req.url))
+        }
+
+        //if the request is for a protected API and the user is not logged in
+        if(isApiRequest && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL('/signin', req.url))
+        }
+   }
+   return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
+};
