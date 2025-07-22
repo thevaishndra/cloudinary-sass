33e77ed28599d8c44b949562ac45af8e64c799e8
thevaishndra <chandrav306m@gmail.com>
2025-07-22 13:17:39 +0530

    uploading on cloudinary

--- sass/app/api/video-upload/route.ts
+++ sass/app/api/video-upload/route.ts
@@ -1,6 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 import {auth} from'@clerk/nextjs/server';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 // Configuration
   cloudinary.config({
@@ -11,46 +14,79 @@ import {auth} from'@clerk/nextjs/server';
 
   interface CloudinaryUploadResult {
     public_id : string;
+    bytes : number;
+    duration?: number
     [key: string]: any
   }
 
 export async function POST(request: NextRequest) {
-    const {userId} = await auth();
-    if(!userId) {
-        return NextResponse.json({error: "Unauthorized"}, {status: 401})
-    }
+
     try {
-        const formData = await request.formData();
-        const file = formData.get('file') as File | null;
+      // const {userId} = await auth();
+      // if(!userId) {
+      //     return NextResponse.json({error: "Unauthorized"}, {status: 401})
+      // }
+      if (
+        !process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ||
+        !process.env.CLOUDINARY_API_KEY ||
+        !process.env.CLOUDINARY_API_SECRET
+      ) {
+        return NextResponse.json(
+          { error: "Cloudinary credentials not found" },
+          { status: 500 }
+        );
+      }
 
-        if(!file) {
-            return NextResponse.json({error: "File not found"}, {status: 400})
-        }
+      const formData = await request.formData();
+      const file = formData.get("file") as File | null;
+      const title = formData.get("title") as string;
+      const description = formData.get("description") as string;
+      const originalSize = formData.get("originalSize") as string;
 
-        const bytes = await file.arrayBuffer();
-        const buffer = Buffer.from(bytes)
+      if (!file) {
+        return NextResponse.json({ error: "File not found" }, { status: 400 });
+      }
 
-        const result = await new Promise<CloudinaryUploadResult>(
-            (resolve, reject) => {
-                const uploadStream = cloudinary.uploader.upload_stream(
-                    {folder: "next-cloudinary-uploads"},
-                    (error, result) => {
-                        if(error) reject(error);
-                        else resolve(result as CloudinaryUploadResult);
-                    }
-                )
-                uploadStream.end(buffer)
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      const result = await new Promise<CloudinaryUploadResult>(
+        (resolve, reject) => {
+          const uploadStream = cloudinary.uploader.upload_stream(
+            { resource_type: "video",
+                folder: "next-cloudinary-uploads",
+                transformation: [
+                    {quality: "auto", fetch_format: "mp4"},
+                ]             
+            },
+            (error, result) => {
+              if (error) reject(error);
+              else resolve(result as CloudinaryUploadResult);
             }
-        )
-        return NextResponse.json(
-            {publicId: result.public_id}, 
-            {status: 200}
-        );
+          );
+          uploadStream.end(buffer);
+        }
+      );
+      const video = await prisma.video.create({
+        data: {
+            title,
+            description,
+            publicId: result.public_id,
+            originalSize: originalSize,
+            compressedSize: String(result.bytes),
+            duration: result.duration || 0,
+        }
+      })
+
+        return NextResponse.json(video)
+
     } catch (error) {
-        console.error("Error uploading image:", error);
-        return NextResponse.json(
-            {error: "Image upload failed"},
+        console.log("Upload video failed", error);
+        return NextResponse.json({error: "Upload video failed"},
             {status: 500}
-        );
+        )
+    } finally {
+        await prisma.$disconnect()
     }
+
 }
